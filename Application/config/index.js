e65c2694b284d4ab4b8a1e00538945918e30e222
Mike J <gufranmirza1@gmail.com>
2019-02-25 04:18:27 +0530

    Add External CSP config

--- Application/config/index.js
+++ Application/config/index.js
@@ -5,3 +5,16 @@ export const title =
   'React Next JS PWA - SEO-Ready, Production-Ready, SSR, Hot-Reload, CSS-in-JS,  SSR Caching';
 
 export const description = '';
+
+export const cspExtensions = {
+  childSrc: [''],
+  connectSrc: [],
+  defaultSrc: [],
+  fontSrc: ['fonts.googleapis.com', 'fonts.gstatic.com', 'cdn.embedly.com'],
+  imgSrc: [],
+  mediaSrc: [],
+  manifestSrc: [],
+  objectSrc: [],
+  scriptSrc: [],
+  styleSrc: []
+};
