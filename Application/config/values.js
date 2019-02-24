3f0e8ae03323973a1fc83e9689db0ab499f848cf
Mike J <gufranmirza1@gmail.com>
2019-02-25 04:32:14 +0530

    Fix Project Config File & Provide Getter Function

---
+++ Application/config/values.js
@@ -0,0 +1,27 @@
+/**
+ * Project Configuration.
+ *
+ * NOTE: All file/folder paths should be relative to the project root. The
+ * absolute paths should be resolved during runtime by our build internal/server.
+ */
+
+const values = {
+  title:
+    'React Next JS PWA - SEO-Ready, Production-Ready, SSR, Hot-Reload, CSS-in-JS,  SSR Caching',
+  description: '',
+  // Content Security Policy (CSP)
+  // @see server/middleware/security for more info.
+  cspExtensions: {
+    childSrc: [],
+    connectSrc: [],
+    defaultSrc: [],
+    fontSrc: [],
+    mediaSrc: [],
+    manifestSrc: [],
+    objectSrc: [],
+    scriptSrc: [],
+    styleSrc: ['fonts.googleapis.com']
+  }
+};
+
+exports.default = values;
